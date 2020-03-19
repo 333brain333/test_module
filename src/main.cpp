951ba7d390a0ec7df1021e5e253adabdb89d21ec
KotelnikovAndrew <222brain222@gmail.com>
2020-03-19 12:35:09 +0300

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,186 @@
+#include <SPI.h>
+#include <mcp2515.h>
+#include "main.h"
+
+
+
+
+void irqHandler()
+{
+  interrupt=true;
+}
+
+void SET_SPEED(void) {
+  
+  if (incoming_ref_speed > 230) {
+    incoming_ref_speed = 230.0;
+  }
+  else if (incoming_ref_speed < 25) {
+    incoming_ref_speed = 25.0;
+  }
+  // take the SS pin low to select the chip:
+  digitalWrite(SS, LOW);
+  //  send in the address and value via SPI:
+  SPI.transfer((byte)0); // address
+  SPI.transfer((byte)incoming_ref_speed + 20); // +20 due to neutral position iт PALESSE FS8060 isn't 256/2=127 but it is 147
+  if (incoming_ref_speed> 125 && incoming_ref_speed< 129) {
+    digitalWrite(relay2, HIGH);
+  }
+  else {
+    digitalWrite(relay2, LOW);
+  }
+  // take the SS pin high to de-select the chip:
+  digitalWrite(SS, HIGH);
+}
+
+
+
+void GET_HANDLE(void)
+{
+  handle_pos = (int)(analogRead(signal_pin) * 0.25);
+}
+
+void GET_WIPER_POS(void)
+{
+  wiper_pos=analogRead(wiper)*0.25;
+}
+
+void SET_MANUAL(void)
+{
+  digitalWrite(relay1, LOW);
+  current_mode = "manual";
+}
+
+void SET_ROBOT()
+{
+  digitalWrite(relay1, HIGH);
+  current_mode = "robot";
+
+}
+
+
+void SEND_FOR_HUMAN()
+{
+  if (millis()-prevSendTime>300){
+    prevSendTime=millis();
+    Serial.print((String)"Handle position: " + handle_pos + ";Pot. value: " + incoming_ref_speed + ";Actual pot.value: "+wiper_pos+";Neutral: " + digitalRead(neutral) + ";Mode: " + current_mode + ";Speed from CAN1: " + String(speedFbCAN) + ";Speed from CAN2: " + String(speedFbCAN_1) +";err: "+errorNames[status]+ "\n");
+  }
+}
+
+void SEND_FOR_PLOTTER(){
+  Serial.print(incoming_ref_speed);
+  Serial.print(" ");
+  Serial.println(wiper_pos);
+}
+
+void readCAN()
+{
+  if (interrupt)
+  {
+    interrupt=false;
+  if ((mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK)) { // && (canMsg.can_id == 0x98FF0102) ) {
+    speedFbCAN = 0.2 * canMsg.data[2];
+  }
+  }
+
+  // delay(100);
+  // if ((mcp2515_1.readMessage(&canMsg) == MCP2515::ERROR_OK)) { // && (canMsg.can_id == 0x98FF0102) ) {
+  //   speedFbCAN_1 = 0.2 * canMsg.data[2];
+  // }
+}
+
+void sendCAN()
+{
+  
+}
+
+int is_in_range(int w){
+  if (w>(incoming_ref_speed+18)&&w<(incoming_ref_speed+20)){
+    return 1;
+  }
+  else {
+    errCounter++;
+    return 0;
+  }
+}
+
+void err_counter_check(){
+  if ((millis()-errTimer)>500){
+    errTimer=millis();
+    if (errCounter>2){
+      status=ERR;
+    }
+    else 
+    {
+      errCounter=0;
+      status=NORMAL;
+    }
+  }
+}
+
+
+//////////////////////////////////////////////////////////////////////SETUP//////////////////////////////////////////////////////////////////////
+
+void setup() {
+
+  SPI.begin();
+  delay(200);
+  pinMode(SS, OUTPUT); // switch chip select pin to output mode
+  pinMode(relay1, OUTPUT); // switch relay 1 pin to output mode
+  pinMode(relay2, OUTPUT);
+  digitalWrite(relay1, LOW);
+  digitalWrite(relay2, LOW);
+  pinMode(neutral, INPUT);
+  digitalWrite(neutral, HIGH);
+  pinMode(interruptPin, INPUT_PULLUP);
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);
+  mcp2515.setNormalMode();
+  mcp2515_1.reset();
+  mcp2515_1.setBitrate(CAN_500KBPS, MCP_8MHZ);
+  mcp2515_1.setNormalMode();
+
+  Serial.begin(115200);
+  Serial.setTimeout(10);
+  while (!Serial) {
+    ;
+  }
+
+  prevSendTime = millis();
+  errTimer = millis();
+  attachInterrupt(digitalPinToInterrupt(interruptPin), irqHandler,FALLING);
+  
+}
+
+//////////////////////////////////////////////////////////////////////LOOP//////////////////////////////////////////////////////////////////////
+
+
+void loop() {
+
+  // Read serial input:
+  if (Serial.available() > 0) {
+    incoming_mes = Serial.readString();
+    if (incoming_mes == "restart") {
+      errCounter=0;
+      status=NORMAL;
+    }
+    else{
+      incoming_ref_speed = incoming_mes.substring(0, 3).toInt();
+      incoming_mode = incoming_mes.substring(3, 4);
+      if (incoming_mode == "m") {
+        SET_MANUAL();
+      }
+      if (incoming_mode == "r") {
+        SET_ROBOT();
+      }
+    }
+  }
+
+  readCAN();
+  GET_HANDLE();
+  SEND_FOR_HUMAN();
+  SET_SPEED();
+  GET_WIPER_POS();
+  is_in_range(wiper_pos);
+  err_counter_check();
+}
---
+++ src/main.h
@@ -0,0 +1,33 @@
+MCP2515 mcp2515(10);
+MCP2515 mcp2515_1(9);
+
+struct can_frame canMsg;
+enum status_enum{
+NORMAL,
+ERR
+};
+enum status_enum status=NORMAL;
+
+String errorNames[] = {
+  "Normal",
+  "ERROR"
+};
+
+double speedFbCAN = 0.0;
+double speedFbCAN_1 = 0.0;
+unsigned long prevSendTime = 0;
+unsigned long errTimer = 0;
+const int relay1 = 5; // relay 1 - according to principal scheme  - it connects "neutral -" line to +24V - in case of HIGH mode, or disconnets with any line in case of LOW mode
+const int relay2 = 6; // relay 2 - according to principal scheme - it connecnts "signal" and "neutral -" lines to handle or to digital potentiometer and to relay 1 accordingly
+const int neutral = 7; // using this pin one could get nutral line state in inversed logic  - "neutral -":+24 V ==> 7 pin is LOW or "neutral -": 0 V ==> 7 pin is HIGH
+const int signal_pin = A0; // using this pin one could get analog value of the handle position. +0.5V - moving forward with max velocity; +2.5V - neutral position, no moving; +4.5V - moving backwards with max velocity
+const int wiper = A5; // wiper of the digital potentiometer. The usage of this pin helps to check if sent reference speed to digital potentiometer is implemented or not.
+const int interruptPin = 2;
+int handle_pos = 0; // position of the handle within a range of 0-255
+int wiper_pos = 0; //here contains position in range 0-255 of the wiper of the digital potentio
+int incoming_ref_speed = 127; //in range 0-255
+int errCounter = 0;
+String incoming_mes = "";
+String incoming_mode = "m";
+String current_mode = "manual";
+bool interrupt = false;
